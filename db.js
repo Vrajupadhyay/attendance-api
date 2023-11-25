9daa22c97fafb63e1dab7ebcaf4e85bad3e7bc46
Vraj Upadhyay <118684944+Vrajupadhyay@users.noreply.github.com>
2023-11-25 17:17:35 +0530

    first

---
+++ db.js
@@ -0,0 +1,92 @@
+// // db.js
+
+// const mysql = require('mysql2');
+
+// // Database configuration
+// const db = mysql.createConnection({
+//   host: process.env.HOST,
+//   port: process.env.DB_PORT, // If 3308 is the correct port    
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+// });
+
+// // Connect to the database
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//   } else {
+//     console.log('Connected to MySQL database');
+//   }
+// });
+
+// module.exports = db;
+
+// const mysql = require('mysql2');
+
+// // Database configuration
+// const db = mysql.createConnection({
+//   host: process.env.HOST,
+//   port: process.env.DB_PORT, // If 3308 is the correct port    
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+//   // Reconnect option with a delay of 2 seconds between reconnect attempts
+//   reconnect: {
+//     autoReconnect: true,
+//     delay: 2000, // milliseconds
+//   },
+// });
+
+// // Function to handle MySQL connection errors
+// function handleDisconnect() {
+//   db.on('error', (err) => {
+//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       console.error('Database connection was closed.');
+//       setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
+//     } else {
+//       throw err;
+//     }
+//   });
+// }
+
+// // Connect to the database and handle disconnections
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
+//   } else {
+//     console.log('Connected to MySQL database');
+//   }
+// });
+
+// module.exports = db;
+
+const mysql = require('mysql2/promise');
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  port: process.env.DB_PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+// Function to handle MySQL connection errors and reconnect
+function handleDisconnect() {
+  pool.on('error', (err) => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
+      setTimeout(handleDisconnect, 2000); // Attempt to reconnect after 2 seconds
+    } else {
+      throw err;
+    }
+  });
+}
+
+// Call the handleDisconnect function to start monitoring for errors
+handleDisconnect();
+
+module.exports = pool;
+
